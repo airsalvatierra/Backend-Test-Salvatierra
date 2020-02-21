6053aea3d2d3e10e2cdc437529fff9eb98cf2e43
David Salvatierra <air.salvatierra@gmail.com>
2020-02-21 17:25:47 -0300

    tempting models

--- mealdelivery/models.py
+++ mealdelivery/models.py
@@ -1,3 +1,94 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.contrib.auth.models import User
+from django.db import models
+from django.utils import timezone
+
+
+# ***Fields from the Auth User Model***
+# username
+# first_name
+# last_name
+# email (Mandatory)
+# password
+# groups
+# user_permissions
+# is_staff
+# is_active
+# is_superuser
+# last_login
+# date_joined
+
+class Employee(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    department = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    address = models.CharField(max_length=300)
+    # control fields
+    created_at = models.DateTimeField(default=timezone.now)
+    creation_user = models.CharField(max_length=250)
+    last_modified = models.DateTimeField(null=True, blank=True)
+    last_user_modified = models.CharField(
+        max_length=250, blank=True, null=True)
+
+    def __str__(self):
+        return "{}".format(self.user)
+
+
+class Menu(models.Model):
+    menu_date = models.DateField('Menu Date')
+    uuid_url = models.UUIDField('UUID', default=uuid.uuid4, unique=True)
+    option1 = models.CharField('Option 1', max_length=250)
+    option2 = models.CharField(
+        'Option 2',
+        max_length=250,
+        blank=True,
+        null=True)
+    option3 = models.CharField(
+        'Option 3',
+        max_length=250,
+        blank=True,
+        null=True)
+    option4 = models.CharField(
+        'Option 4',
+        max_length=250,
+        blank=True,
+        null=True)
+    # control fields
+    created_at = models.DateTimeField(default=timezone.now)
+    creation_user = models.CharField(max_length=250)
+    last_modified = models.DateTimeField(null=True, blank=True)
+    last_user_modified = models.CharField(
+        max_length=250, blank=True, null=True)
+
+    def __str__(self):
+        return "Menu - {}".format(self.menu_date.strftime('%m/%d/%Y'))
+
+
+# class MenuDetail(models.Model):
+#     menu = models.ForeignKey('Menu', on_delete=models.CASCADE,)
+#     option = models.CharField(max_length=250)
+#     # control fields
+#     created_at = models.DateTimeField(default=timezone.now)
+#     creation_user = models.CharField(max_length=250)
+#     last_modified = models.DateTimeField(null=True, blank=True)
+#     last_user_modified = models.CharField(
+#         max_length=250, blank=True, null=True)
+
+
+class MenuEmployee(models.Model):
+    employee = models.CharField(max_length=50)
+    menu_date = models.DateField('Menu Date')
+    option_selected = models.CharField(max_length=250)
+    customization = models.CharField(max_length=500)
+    # control fields
+    created_at = models.DateTimeField(default=timezone.now)
+    creation_user = models.CharField(max_length=250)
+    last_modified = models.DateTimeField(null=True, blank=True)
+    last_user_modified = models.CharField(
+        max_length=250, blank=True, null=True)
+
+    def __str__(self):
+        return "{} - {} - {}".format(self.employee,
+                                     self.menu_date.strftime('%m/%d/%Y'),
+                                     self.option_selected)
