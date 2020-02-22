405b3193bbd02e65484395f41cd1a1c4c04d9ad1
David Salvatierra <air.salvatierra@gmail.com>
2020-02-22 19:45:41 -0300

    models updated

--- mealdelivery/models.py
+++ mealdelivery/models.py
@@ -1,8 +1,6 @@
 import uuid
 
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 from django.db import models
 from django.utils import timezone
 
