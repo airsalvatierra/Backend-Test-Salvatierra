b38aafd3b991a3ca8b7de5d3fb8f4d2a179aff42
David Salvatierra <air.salvatierra@gmail.com>
2020-02-22 20:13:15 -0300

    add template tag to filter menus

---
+++ menumaker/templatetags/app_filters.py
@@ -0,0 +1,9 @@
+from django import template
+
+from comun.utils import check_if_is_supervisor
+
+register = template.Library()
+
+@register.simple_tag(takes_context=True)
+def show_supervisor_menus(context):
+    return check_if_is_supervisor(context['request'].user.username)
