a52a14b4e3dbd4c458d0486e636402b38020173c
David Salvatierra <air.salvatierra@gmail.com>
2020-02-22 01:00:18 -0300

    settings and urls modified

--- menumaker/urls.py
+++ menumaker/urls.py
@@ -19,17 +19,52 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import include, path
 
-from mealdelivery.views import home_page, user_logout, LoginView, change_password
+from mealdelivery.views import (home_page, user_logout, LoginView,
+                                change_password, change_password_mandatory)
 
 urlpatterns = [
-    path('', home_page, name='home'),
-    path('admin/', admin.site.urls),
-    path('delivery/', include('mealdelivery.urls')),
-    path('user_login/', LoginView.as_view(), name='user_login'),
-    path('logout/', user_logout, name='logout'),
-    path('password/', change_password, name='change_password'),
-    path('accounts/password_reset/', auth_views.PasswordResetView.as_view(),name='password_reset'),
-    path('accounts/password_reset/done/', auth_views.PasswordResetDoneView.as_view(),name='password_reset_done'),
-    path('accounts/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
-    path('accounts/reset/done/', auth_views.PasswordResetCompleteView.as_view(),name='password_reset_complete'),    
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path(
+        '',
+        home_page,
+        name='home'),
+    path(
+        'admin/',
+        admin.site.urls),
+    path(
+        'delivery/',
+        include('mealdelivery.urls')),
+    path(
+        'user_login/',
+        LoginView.as_view(),
+        name='user_login'),
+    path(
+        'logout/',
+        user_logout,
+        name='logout'),
+    path(
+        'password/',
+        change_password,
+        name='change_password'),
+    path(
+        'password2/',
+        change_password_mandatory,
+        name='change_password2'),
+    path(
+        'accounts/password_reset/',
+        auth_views.PasswordResetView.as_view(),
+        name='password_reset'),
+    path(
+        'accounts/password_reset/done/',
+        auth_views.PasswordResetDoneView.as_view(),
+        name='password_reset_done'),
+    path(
+        'accounts/reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(),
+        name='password_reset_confirm'),
+    path(
+        'accounts/reset/done/',
+        auth_views.PasswordResetCompleteView.as_view(),
+        name='password_reset_complete'),
+] + static(
+    settings.STATIC_URL,
+    document_root=settings.STATIC_ROOT)
