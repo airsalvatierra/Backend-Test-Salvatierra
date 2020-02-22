e4e0401d080d260a52df0893e3303991245c510d
David Salvatierra <air.salvatierra@gmail.com>
2020-02-22 19:45:04 -0300

    urls updated

--- mealdelivery/urls.py
+++ mealdelivery/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
 
-from .views import CreateMenuView, ListMenusView, EditMenuView
+from .views import (CreateMenuView, ListMenusView, EditMenuView,
+                    CreateEmployeeView, select_menu, MenuSelectedView)
 
 
 app_name = 'mealdelivery'
 
 urlpatterns = [
-    path('newmenu', CreateMenuView.as_view(), name='newmenu'),
-    path('list_menus', ListMenusView.as_view(), name='list_menus'),
+    path('new_menu/', CreateMenuView.as_view(), name='new_menu'),
+    path('list_menus/', ListMenusView.as_view(), name='list_menus'),
     path('list_menus/<int:pk>/', EditMenuView.as_view(), name='edit_menu'),
+    path('new_employee/', CreateEmployeeView.as_view(), name='new_employee'),
+    path('select_menu/', select_menu, name='select_menu'),
+    path('menus_selected/', MenuSelectedView.as_view(), name='menus_selected'),
 ]
