48a7a1dab5102b97b72404f3cea303e3c987e4f3
David Salvatierra <air.salvatierra@gmail.com>
2020-02-22 19:47:30 -0300

    settigns updated with santiago time zone

--- menumaker/settings.py
+++ menumaker/settings.py
@@ -127,13 +127,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
+TIME_ZONE = 'America/Santiago'
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 LOGIN_URL = '/user_login'
