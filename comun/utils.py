9b2cb13cef56cf30d356e6463ece4810762a6573
David Salvatierra <air.salvatierra@gmail.com>
2020-02-23 00:46:19 -0300

    minor corrections on utils

--- comun/utils.py
+++ comun/utils.py
@@ -1,12 +1,16 @@
 import os
 from datetime import datetime
+import pytz
 
 from django.contrib.auth.models import User
 from django.conf import settings
 import slack
 
-from .constants import ALLOW_VIEW_SELECTION
 from mealdelivery.models import Employee, Menu
+from .constants import ALLOW_VIEW_SELECTION
+
+tz = pytz.timezone('America/Santiago')
+
 
 def check_if_is_supervisor(username):
     try:
@@ -14,6 +18,7 @@ def check_if_is_supervisor(username):
     except User.DoesNotExist:
         return False
     return user.username in ALLOW_VIEW_SELECTION
+
 
 def send_slack_reminder(message, country=None):
     # get employees
@@ -23,7 +28,7 @@ def send_slack_reminder(message, country=None):
         employees = Employee.objects.all()
 
     # loop the employees and add reminder
-    today = datetime.now()
+    today = datetime.now(tz)
     date_time = datetime(today.year, today.month, today.day, 10)
     client = slack.WebClient(token=os.environ['SLACK_API_TOKEN'])
     errors = []
@@ -40,6 +45,7 @@ def send_slack_reminder(message, country=None):
         return errors
     return 'ok'
 
+
 def get_slack_reminder_message(today):
     try:
         menu = Menu.objects.get(menu_date=today)
@@ -47,10 +53,10 @@ def get_slack_reminder_message(today):
         return None
 
     if settings.DEBUG:
-        url = "http://127.0.0.1:8000/{}".format(menu.uuid_url)
+        url = "http://127.0.0.1:8000/menu/{}".format(menu.uuid_url)
     else:
         url = "https://nora.cornershop.io/menu/{}".format(menu.uuid_url)
 
-    message = 'Remeber to choose your meal \n\n {url}'
+    message = 'Remeber to choose your meal {}'.format(url)
 
     return message
